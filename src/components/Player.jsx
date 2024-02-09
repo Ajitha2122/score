1f9899037bc51609f2d302f925b9fa5a14338049
Ajitha2122 <ajitha@apptino.com>
2024-02-09 18:45:09 +0530

    timer

---
+++ src/components/Player.jsx
@@ -0,0 +1,20 @@
+import { useState, useRef } from "react";
+
+export default function Player() {
+  const playerName = useRef();
+  const [enterPlayerName, setEnterPlayerName] = useState(null);
+
+  function handleClick() {
+    setEnterPlayerName(playerName.current.value);
+    playerName.current.value = " ";
+  }
+  return (
+    <section id="player">
+      <h2>Welcome {enterPlayerName ?? "unKnown entity"}</h2>
+      <p>
+        <input type="text" ref={playerName} />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,31 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+const ResultModal = forwardRef(function ResultModal(
+  { result, targetTime },
+  ref
+) {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return (
+    <dialog ref={ref} className="result-modal">
+      <h2>you {result}</h2>
+      <p>
+        the target time was<strong>{targetTime} seconds</strong>
+      </p>
+      <p>
+        you stopped the timer with <strong>x seconds left</strong>
+      </p>
+      <form method="dialog">
+        <button>close</button>
+      </form>
+    </dialog>
+  );
+});
+export default ResultModal;
